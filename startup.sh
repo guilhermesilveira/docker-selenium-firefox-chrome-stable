3ee669c6e458494a8432d05d2bb77b53b751ec5f
guilherme <guilherme.silveira@caelum.com.br>
2014-09-11 11:03:54 -0300

    info on how to run

--- startup.sh
+++
@@ -1,4 +0,0 @@
-docker run -d --name selenium -p 5999:5999 -p 4444:4444 -t -i guilhermesilveira/selenium
-echo "$(boot2docker ip 2>/dev/null):5999"
-open http://$(boot2docker ip 2>/dev/null):4444/wd/hub
-docker logs -f selenium
